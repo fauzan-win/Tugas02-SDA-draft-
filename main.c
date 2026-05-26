53bee66404b2b730e78894cbb1d8bd506caa4a86
Althaffia <althafpuput@gmail.com>
2026-05-26 15:35:03 +0700

    update tugas

---
+++ main.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+int main() {
+    int pilihan;
+
+    printf("=====MENU UTAMA=====");
+    printf("1. Sorting Dasar");
+    printf("2. Advance Sorting");
+    printf("3. Keluar");
+    printf("Pilih menu :");
+    scanf("%d", &pilihan);
+
+    if (pilihan = 1)
+    {
+        printf("=====SORTING DASAR=====");
+        printf("1. Bubble Sort");
+        printf("2. Insertion Sort");
+        printf("3. Selection Sort");
+        printf("4. Kembali");
+
+    }
+
+    if (pilihan = 2)
+    {
+        
+    }
+    
+    
+
+    return 0;
+}
