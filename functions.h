962765c1e5084979bcfcc155814f0ebcd838611a
zanbyuu <fauzana@mhs.usk.ac.id>
2026-05-27 14:41:22 +0700

    bagi

--- functions.h
+++ functions.h
@@ -29,13 +29,13 @@ void shuffleStr(char arr[][MAX_PANJANG_KATA], int n);
 
 // Membaca kata dari words.txt
 int bacaFile(char arr[][MAX_PANJANG_KATA], int maks);
+
 void merge(char arr[][MAX_PANJANG_KATA], int kiri, int tengah, int kanan);
 void mergeSort(char arr[][MAX_PANJANG_KATA], int kiri, int kanan);
 int  partisi(char arr[][MAX_PANJANG_KATA], int rendah, int tinggi);
 void quickSort(char arr[][MAX_PANJANG_KATA], int rendah, int tinggi);
 void shellSort(char arr[][MAX_PANJANG_KATA], int n);
 void jalankanAdvanceSorting(int pilihan);
-
 
 //Deklarasi fungsi menu dari main.c
 void menuSortingDasar(void);
